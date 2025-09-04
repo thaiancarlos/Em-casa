3881eaa67548915ac13b9a69bd53af4bb30dbaa9
thaiancarlos <thaiancarlosolivera@gmail.com>
2025-09-04 20:05:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Aguarda 7 segundos (quase no final da animação do sol) e exibe o nome da empresa
+setTimeout(() => {
+  const name = document.getElementById('companyName');
+  name.style.opacity = 1;
+}, 7000);
